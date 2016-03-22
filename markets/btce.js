ed18458e4eda2bba0e4d437fc5825b186ff26e29
Kirill <infloop.gm@gmail.com>
2016-03-23 01:19:57 +0300

    Update btce.js

--- markets/btce.js
+++ markets/btce.js
@@ -9,7 +9,7 @@ var Promise = require("bluebird");
 /**
  * @type {BTCE}
  */
-var BTCEAsync = Promise.promisifyAll(new BTCE("3DG426VG-43FO0QW8-OIEIQ5E9-8IRBU5T5-YSG5D5BU", "b8d9077212a3d218d09165ef993e82e97906db5cb684298e2510d71ee78272bb"));
+var BTCEAsync = Promise.promisifyAll(new BTCE());
 var marketBTCE = new Market('BTC-E');
 
 marketBTCE.addPair(new Pair('ltc_rur', Currencies.LTC, Currencies.RUR));
